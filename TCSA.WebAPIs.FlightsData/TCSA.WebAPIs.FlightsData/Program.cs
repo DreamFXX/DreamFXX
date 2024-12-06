d1fc8ca2e1e3f3647a5094e3f043ce8a63267340
DreamFXX <filipek.husak@gmail.com>
2024-12-06 20:19:01 +0100

    add

--- TCSA.WebAPIs.FlightsData/TCSA.WebAPIs.FlightsData/Program.cs
+++ TCSA.WebAPIs.FlightsData/TCSA.WebAPIs.FlightsData/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<F>;
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
