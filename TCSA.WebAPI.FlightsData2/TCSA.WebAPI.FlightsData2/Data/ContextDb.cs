2b3bf2f03c70dd90efea9558422ee2b375a473aa
DreamFXX <filipek.husak@gmail.com>
2024-12-07 21:42:55 +0100

    EF + Sqlite

---
+++ TCSA.WebAPI.FlightsData2/TCSA.WebAPI.FlightsData2/Data/ContextDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TCSA.WebAPI.FlightsData2.Data;
+public class FlightsDbContext2 : DbContext
+{
+    public FlightsDbContext2(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<FlightsDbContext2> Flights { get; set; }
+}
---
+++ TCSA.WebAPI.FlightsData2/TCSA.WebAPI.FlightsData2/Program.cs
@@ -0,0 +1,23 @@
+using TCSA.WebAPI.FlightsData2.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<FlightsDbContext2>(opt => opt.UseSqlite());
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapOpenApi();
+
+app.Run();
