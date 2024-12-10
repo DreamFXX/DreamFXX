12158ae62524c567f1e0097e258366557400c5b4
DreamFXX <filipek.husak@gmail.com>
2024-12-10 19:25:04 +0100

    Basic structure created

---
+++ HabitTracker_Advanced/HabitTracker_Advanced/HabitTrackerMain.cs
@@ -0,0 +1,19 @@
+﻿using HabitTracker_Advanced.HabitTracker_Library;
+
+namespace HabitTracker_Advanced;
+
+internal class HabitTrackerMain
+{
+    static DatabaseManager DbManager = new DatabaseManager();
+
+    private static bool exit;
+
+    private static void Main(string[] args)
+    {
+        while (!exit)
+        {
+
+        }
+    }
+}
+
--- _SubstanceUseTracker/README.md
+++ _SubstanceUseTracker/README.md
@@ -1,5 +1,3 @@
 # Substance Use Tracker
 
-Substance Use Tracker is my first personal project written in C#
-
-Currently Im working on functional Console App, then I want to update/learn blazor .net
+Substance Use Tracker is my first personal project written in C#;
