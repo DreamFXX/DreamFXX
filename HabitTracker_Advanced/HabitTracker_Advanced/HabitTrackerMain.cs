675e29116e9b41cd6f5f34c07aae8bed8bc8e08d
DreamFXX <filipek.husak@gmail.com>
2024-12-10 22:38:08 +0100

    connectionString Ok

--- HabitTracker_Advanced/HabitTracker_Advanced/HabitTrackerMain.cs
+++ HabitTracker_Advanced/HabitTracker_Advanced/HabitTrackerMain.cs
@@ -1,10 +1,11 @@
-﻿using HabitTracker_Advanced.HabitTracker_Library;
+﻿using System.Data.SQLite;
+using HabitTracker_Advanced.HabitTracker_Library;
 
 namespace HabitTracker_Advanced;
 
 internal class HabitTrackerMain
 {
-    static DatabaseManager DbManager = new DatabaseManager();
+    static DatabaseManager dbManager = new DatabaseManager();
 
     private static bool exit;
 
@@ -12,8 +13,31 @@ internal class HabitTrackerMain
     {
         while (!exit)
         {
+            Console.WriteLine("\n-- WELCOME IN HABIT TRACKER ADVANCED --\n");
+            //dbManager.CreateDatabaseIfNotExists();
+            ShowMenu();
+            //string menuChoice;
 
         }
+
     }
+
+    private static void ShowMenu()
+    {
+        Console.WriteLine("Please enter one of the following options:\n" +
+                          "===============================\n" +
+                          "\t'c' - Create a habit\n" +
+                          "\t'v' - View habit\n" +
+                          "\t'u' - Update habit\n" +
+                          "\t'i' - Insert into habit\n" +
+                          "\t'd' - Delete from habit\n" +
+                          "\t'r' - Create a report\n" +
+                          "===============================\n" +
+                          "'exit' - Close the application\n" +
+                          "===============================\n" +
+                          "Your choice: \n");
+    }
+
+
 }
 
--- HabitTracker_Advanced/HabitTracker_Advanced/HabitTracker_Library/DatabaseManager.cs
+++ HabitTracker_Advanced/HabitTracker_Advanced/HabitTracker_Library/DatabaseManager.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
+using System.Data.SQLite;
+using System.Configuration;
 
-namespace HabitTracker_Advanced.HabitTracker_Library
+namespace HabitTracker_Advanced.HabitTracker_Library;
+
+public class DatabaseManager
 {
-    internal class DatabaseManager
+    private static readonly string connectionString = LoadCnnString();
+    public static List<string> currentHabits = [];
+
+
+    private static string LoadCnnString(string id = "Default")
     {
+        return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+
+        //// Build a configuration object from JSON file
+        //IConfiguration config = new ConfigurationBuilder()
+        //    .AddJsonFile("appsettings.json")
+        //    .Build();
+
+        //// Get a configuration section
+        //IConfigurationSection section = config.GetSection("ConnectionStrings");
+
+        //// Read simple values
+        //string? connectionString = section.GetConnectionString("DefaultConnection");
+
+        //return connectionString;
     }
+
 }
+
