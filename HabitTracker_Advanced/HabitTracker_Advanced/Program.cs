5b4e1687b65b324e8a1d906b34dc69c82caf1780
DreamFXX <filipek.husak@gmail.com>
2024-12-10 18:51:59 +0100

    new version (final practice) habittracker created

---
+++ HabitTracker_Advanced/HabitTracker_Advanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HabitTracker_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
